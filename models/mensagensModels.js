a1e735fa0832acd40689c97505f7e92ce11fa4f2
Arthur <arthurmarquesazevedo@gmail.com>
2024-06-23 10:00:48 -0300

    Feat: Mensagens model

---
+++ models/mensagensModels.js
@@ -0,0 +1,36 @@
+const pool = require('../config/db');
+
+const getMensagens = async () => {
+    const [rows] = await pool.query('SELECT * FROM TBL_Mensagens');
+    return rows;
+}
+
+const getMensagemById = async (id) => {
+    const [rows] = await pool.query('SELECT * FROM TBL_Mensagens WHERE ID_Mensagem = ?', [id]);
+    return rows;
+}
+
+const createMensagem = async (mensagem) => {
+    const { ID_Usuario, ID_Chat, Mensagem } = mensagem;
+    const [result] = await pool.query(
+        'INSERT INTO TBL_Mensagens (Nome, Email, DDD, Telefone, Mensagem) VALUES (?, ?, ?, ?, ?)',
+        [Nome, Email, DDD, Telefone, Mensagem]
+    );
+    return result.insertId;
+};
+
+const updateMensagem = async (id, mensagem) => {
+    const { ID_Usuario, ID_Chat, Mensagem } = mensagem;
+    const [result] = await pool.query(
+        'UPDATE TBL_Mensagens SET Nome = ?, Email = ?, DDD = ?, Telefone = ?, Mensagem = ? WHERE ID_Mensagem = ?',
+        [Nome, Email, DDD, Telefone, Mensagem]
+    );
+    return result.affectedRows;
+};
+
+const deleteMensagem = async (id) => {
+    const [result] = await pool.query('DELETE FROM TBL_Mensagens WHERE ID_Mensagem = ?', [id]);
+    return result.affectedRows;
+};
+
+module.exports = { getMensagens, getMensagemById, createMensagem, updateMensagem, deleteMensagem };
