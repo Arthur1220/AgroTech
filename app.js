d7793fe1d47bf2860cf871428e157896c5d3f908
Arthur <arthurmarquesazevedo@gmail.com>
2024-06-29 12:14:39 -0300

    Feat: Added funcionario

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/login', require('./routes/login'));
 app.use('/cliente', require('./routes/cliente'));
 app.use('/pedidoSuporte', require('./routes/pedidoSuporte'));
 app.use('/viewSuporte', require('./routes/viewSuporte'));
+app.use('/funcionario', require('./routes/funcionario'));
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 8000;
