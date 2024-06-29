4409a90af4f1fed4abacbbdda3dc85ecd88783f7
Arthur <arthurmarquesazevedo@gmail.com>
2024-06-29 12:09:13 -0300

    Feat: viewSuporte created

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use('/contato', require('./routes/contato'));
 app.use('/login', require('./routes/login'));
 app.use('/cliente', require('./routes/cliente'));
 app.use('/pedidoSuporte', require('./routes/pedidoSuporte'));
+app.use('/viewSuporte', require('./routes/viewSuporte'));
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 8000;
